8f8143081d0d4bc7315a5672a29d6174b98577d4
JavaScript <javascript@Admins-iMac-4.local>
2020-01-07 20:53:32 +0200

    2020

---
+++ exercitiu\/hangman/hangman.js
@@ -0,0 +1,25 @@
+var game = {
+    words: ['apple', 'orange', 'banana'],
+    word: undefined,
+    letters: [],
+    triesLeft: 6;
+    start: function() {
+        this.word = this.randomWord();
+    }
+},
+randomWord: function() {
+    let index = Math.floor(Math.random() * this.words.lenght);
+    return this.words[index];
+},
+tryLetter: function(letter) {
+    this.letter.push(letter);
+    if(this.word.split('').includes(letter)) {
+        this.triesLeft--;
+    }   
+},
+isDead: function() {
+    return this.triesLeft == 0;
+},
+isWinner: function() {
+    
+}
